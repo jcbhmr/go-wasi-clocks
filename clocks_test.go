315cc4e27596f5238c2a4f2061315d467b655d7f
Jacob Hummer <jcbhmr@outlook.com>
2025-09-03 05:01:02 +0000

    save

---
+++ clocks_test.go
@@ -0,0 +1,8 @@
+//go:generate rm -rf ./.out/bindings/ ./monotonic-clock/ ./wall-clock/
+//go:generate go tool wit-bindgen-go generate --out ./.out/bindings/ ./wit/
+//go:generate mv ./.out/bindings/wasi/clocks/monotonic-clock/ ./monotonic-clock/
+//go:generate mv ./.out/bindings/wasi/clocks/wall-clock/ ./wall-clock/
+//go:generate rm -rf ./monotonic-clock/empty.s ./wall-clock/empty.s
+//go:generate go tool jet -g "*.go" -e "DO NOT EDIT.\n" "DO NOT EDIT.\n\n//go:build wasip2\n" -e "\\.out/bindings/wasi/clocks/" "" ./monotonic-clock/ ./wall-clock/
+
+package clocks_test
